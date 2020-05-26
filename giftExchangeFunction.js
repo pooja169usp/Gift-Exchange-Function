bd01fb1544bf4e1dfd2ce3d30d62b348d8eebd01
Pooja Maheshwar Guggari <pooja.maheshwar.guggari@gmail.com>
2020-05-26 18:12:08 -0400

    edits to readme

--- giftExchangeFunction.js
+++ giftExchangeFunction.js
@@ -1,24 +1,3 @@
-// Instructions:
-
-// You are tasked with organizing a gift exchange among friends; every member of the group has 
-// to both give, and receive, a gift from among the others.
- 
-// The only rule is that one can not receive a gift from the same person to whom they give a gift.
- 
-// Write a solution for the above requirements for groups of varying sizes using Javascript. 
-// Remember to account for edge cases that would make the above rules impossible to follow.
-
-// example input:
-// ['Susan', 'Beth', 'Abe', 'Ardi', 'Quan']
- 
-// example output:
-// Abe gives a gift to Quan
-// Ardi gives a gift to Beth
-// Susan gives a gift to Abe
-// Quan gives a gift to Ardi
-// Beth gives a gift to Susan
-
-
 // A function to randomly shuffle the given array
 function shuffleInput(arr) {
     let start = arr.length - 1
